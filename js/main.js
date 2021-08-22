d3067b591a7ea21aec59923bbee2eb3eb1824a12
aelgmal7 <aelgmal7@gmail.com>
2021-08-22 13:35:49 +0200

    thanks reem

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("hellow")
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+  document.getElementById("myBtn").addEventListener('click',function(){
+
+      window.scrollTo({
+          top:0,
+          behavior:"smooth"
+        })
+    })
