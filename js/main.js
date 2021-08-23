a202044a1c69050d54a5ad6b08cbb009e618a85b
aelgmal7 <aelgmal7@gmail.com>
2021-08-23 05:48:57 +0200

    brands js added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 console.log("hellow")
+const brandSection = document.querySelector('#brand-container')
 
 document.getElementById("myBtn").addEventListener('click',function(){
 
@@ -18,32 +19,49 @@ document.getElementById("myBtn").addEventListener('click',function(){
       backSpeed:20
     });
 
-
+    // brands data
     const brands = [
       {
         brandName: 'Toshiba',
-        phoneNumber: '34131318313',
-        imgSrc: 'assests/Samsung_logo_blue.png'
+        phoneNumber: '01129379928',
+        brandLogo : 'assests/Samsung_logo_blue.png' ,
+        imgSrc: [
+          
+         'assests/Samsung_logo_blue.png',
+         'assests/شركة-يونيفرسال.jpg',
+         'assests/لوجو.png'
+        ]
       }
     ]
+    let renderedBrands = []
+    const showBrands = () => {
 
-    const getPrefix = () => {
+    brands.map(b =>{
+     const tmp = getPrefix(b.brandLogo,b.phoneNumber,b.imgSrc)
+     renderedBrands.push(tmp)
+    })
+  }
+
+    const getPrefix = (brandLogo,phoneNumber,imgSrc) => {
+      console.log(brandLogo,phoneNumber,imgSrc)
       return `
       <div class="container slider-container">
       <div class="row">
         <div class="col-4">
+        <div class="container">
           <div class="row text-center mt-lg-5 mt-md-4 mt-sm-3">
             <div class="col-12">
-              <img src="assests/Samsung_logo_blue.png" class="img-fluid" />
+              <img src="${brandLogo}" class="img-fluid" />
               <hr class="w-75 m-auto mb-4 opacity-40" />
             </div>
             <div class="col-12">
-              <a class="contactBtn btn btn-primary" href="tel:01006831361">
-                <h4>تواصل معنا</h4>
-                <span class="text-muted">115668453465</span>
+              <a class="contactBtn btn btn-primary" href="tel:${phoneNumber}">
+                <h4>اتصل الأن</h4>
+                <span  style="color:orange">${phoneNumber}</span>
               </a>
             </div>
           </div>
+        </div>
         </div>
         <div class="col-8">
           <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
@@ -54,13 +72,13 @@ document.getElementById("myBtn").addEventListener('click',function(){
             </div>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src="assests/شركة-يونيفرسال.jpg" class="d-block w-100" alt="...">
+                <img src="${imgSrc[0]}" class="d-block w-100" alt="...">
               </div>
               <div class="carousel-item">
-                <img src="assests/be938c35ae21acb8d4fae8035f30746d.png" class="d-block w-100" alt="...">
+                <img src="${imgSrc[1]}" class="d-block w-100" alt="...">
               </div>
               <div class="carousel-item">
-                <img src="assests/195-1958564_lg-logo-png-lg-logo.png" class="d-block w-100" alt="...">
+                <img src="${imgSrc[2]}" class="d-block w-100" alt="...">
               </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -78,18 +96,10 @@ document.getElementById("myBtn").addEventListener('click',function(){
       `
     } 
 
-    const getPostfix = () => {
-      return `
-      </div>
-      </div>
-    </div>
-  </div>
-      `
-    }
 
-    const brandSection = document.querySelector('#brand-container')
     console.log("🚀 ~ file: main.js ~ line 91 ~ brandSection", brandSection)
 
     window.addEventListener('load', () => {
-      brandSection.innerHTML = getPrefix()
+      showBrands()
+      brandSection.innerHTML = renderedBrands
     })
