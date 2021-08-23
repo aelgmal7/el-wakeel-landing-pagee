e886bd73e6a493e78f1554bc40cf49471745fb19
aelgmal7 <aelgmal7@gmail.com>
2021-08-23 11:12:49 +0200

    brand slider

---
+++ js/brands.js
@@ -0,0 +1,46 @@
+const x = [
+    {
+      brandName: 'Toshiba',
+      phoneNumber: '01129379928',
+      brandLogo : 'assests/Samsung_logo_blue.png' ,
+      imgSrc: [
+        
+       'assests/Samsung_logo_blue.png',
+       'assests/شركة-يونيفرسال.jpg',
+       'assests/لوجو.png'
+      ]
+    },
+    {
+      brandName: 'Toshiba',
+      phoneNumber: '01129379928',
+      brandLogo : 'assests/Samsung_logo_blue.png' ,
+      imgSrc: [
+        
+       'assests/Samsung_logo_blue.png',
+       'assests/شركة-يونيفرسال.jpg',
+       'assests/لوجو.png'
+      ]
+    },
+    {
+      brandName: 'Toshiba',
+      phoneNumber: '01129379928',
+      brandLogo : 'assests/Samsung_logo_blue.png' ,
+      imgSrc: [
+        
+       'assests/Samsung_logo_blue.png',
+       'assests/شركة-يونيفرسال.jpg',
+       'assests/لوجو.png'
+      ]
+    }, {
+      brandName: 'Toshiba',
+      phoneNumber: '01129379928',
+      brandLogo : 'assests/Samsung_logo_blue.png' ,
+      imgSrc: [
+        
+       'assests/Samsung_logo_blue.png',
+       'assests/شركة-يونيفرسال.jpg',
+       'assests/لوجو.png'
+      ]
+    }
+  ]
+  export default x ;
--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ document.getElementById("myBtn").addEventListener('click',function(){
     //   shuffle:true,
       backSpeed:20
     });
-
+  let toggleBrandShow = false
     // brands data
     const brands = [
       {
@@ -31,21 +31,59 @@ document.getElementById("myBtn").addEventListener('click',function(){
          'assests/شركة-يونيفرسال.jpg',
          'assests/لوجو.png'
         ]
+      },
+      {
+        brandName: 'Toshiba',
+        phoneNumber: '01129379928',
+        brandLogo : 'assests/Samsung_logo_blue.png' ,
+        imgSrc: [
+          
+         'assests/Samsung_logo_blue.png',
+         'assests/شركة-يونيفرسال.jpg',
+         'assests/لوجو.png'
+        ]
+      },
+      {
+        brandName: 'Toshiba',
+        phoneNumber: '01129379928',
+        brandLogo : 'assests/Samsung_logo_blue.png' ,
+        imgSrc: [
+          
+         'assests/Samsung_logo_blue.png',
+         'assests/شركة-يونيفرسال.jpg',
+         'assests/لوجو.png'
+        ]
+      }, {
+        brandName: 'Toshiba',
+        phoneNumber: '01129379928',
+        brandLogo : 'assests/Samsung_logo_blue.png' ,
+        imgSrc: [
+          
+         'assests/Samsung_logo_blue.png',
+         'assests/شركة-يونيفرسال.jpg',
+         'assests/لوجو.png'
+        ]
       }
     ]
-    let renderedBrands = []
+    let count =0
+    let renderedBrands = ''
     const showBrands = () => {
 
     brands.map(b =>{
-     const tmp = getPrefix(b.brandLogo,b.phoneNumber,b.imgSrc)
-     renderedBrands.push(tmp)
+     const tmp = toggleBrandShow?  getPrefix(b.brandLogo,b.phoneNumber,b.imgSrc) : getPostfix(b.brandLogo,b.phoneNumber,b.imgSrc)
+    //  renderedBrands.push(tmp)
+    renderedBrands += tmp
+    count++;
+
+      // toggleBrandShow = !toggleBrandShow
     })
   }
 
     const getPrefix = (brandLogo,phoneNumber,imgSrc) => {
       console.log(brandLogo,phoneNumber,imgSrc)
       return `
-      <div class="container slider-container">
+      
+      <div class="container slider-container mb-5">
       <div class="row">
         <div class="col-4">
         <div class="container">
@@ -64,7 +102,7 @@ document.getElementById("myBtn").addEventListener('click',function(){
         </div>
         </div>
         <div class="col-8">
-          <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+          <div id="carouselExampleIndicators-${count}" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -81,11 +119,11 @@ document.getElementById("myBtn").addEventListener('click',function(){
                 <img src="${imgSrc[2]}" class="d-block w-100" alt="...">
               </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators-${count}" data-bs-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators-${count}" data-bs-slide="next">
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="visually-hidden">Next</span>
             </button>
@@ -93,6 +131,60 @@ document.getElementById("myBtn").addEventListener('click',function(){
         </div>
       </div>
     </div>
+      `
+    } 
+const getPostfix = (brandLogo,phoneNumber,imgSrc) => {
+      console.log(brandLogo,phoneNumber,imgSrc)
+      return `
+      <div class="container slider-container mb-5">
+      <div class="row">
+        <div class="col-8">
+          <div id="carouselExampleIndicators-${count}" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="${imgSrc[0]}" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="${imgSrc[1]}" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="${imgSrc[2]}" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators-${count}" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators-${count}" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+        <div class="col-4">
+        <div class="container">
+          <div class="row text-center mt-lg-5 mt-md-4 mt-sm-3">
+            <div class="col-12">
+              <img src="${brandLogo}" class="img-fluid" />
+              <hr class="w-75 m-auto mb-4 opacity-40" />
+            </div>
+            <div class="col-12">
+              <a class="contactBtn btn btn-primary" href="tel:${phoneNumber}">
+                <h4>اتصل الأن</h4>
+                <span  style="color:orange">${phoneNumber}</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+
       `
     } 
 
